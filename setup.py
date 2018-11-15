a5906d7904749aa8042a5d068918d88a61a123f8
saurabh <saurabhrane.m@gmail.com>
2018-11-15 15:21:40 +0530

    lib name changed

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py_advanced_search_list",
-    version="0.0.2",
+    name="py_advanced_search_list_prod",
+    version="0.0.1",
     author="Saurabh",
     author_email="saurabhrane.m@gmail.com",
     description="Package to advance search",
